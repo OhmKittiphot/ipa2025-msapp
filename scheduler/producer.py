8e41ceadc4f18d9c6095b7962dd488c2ac3add74
RomieOhm <66070239@kmitl.ac.th>
2025-09-08 23:36:57 +0700

    S4-S5

--- scheduler/producer.py
+++ scheduler/producer.py
@@ -9,12 +9,13 @@ def produce(host, body):
     credentials = pika.PlainCredentials(rabbitmq_user, rabbitmq_pass)
     parameters = pika.ConnectionParameters(host, credentials=credentials)
     connection = pika.BlockingConnection(parameters)
-    
     channel = connection.channel()
 
     channel.exchange_declare(exchange="jobs", exchange_type="direct")
     channel.queue_declare(queue="router_jobs")
-    channel.queue_bind(queue="router_jobs", exchange="jobs", routing_key="check_interfaces")
+    channel.queue_bind(
+        queue="router_jobs", exchange="jobs", routing_key="check_interfaces"
+    )
 
     channel.basic_publish(exchange="jobs", routing_key="check_interfaces", body=body)
 
@@ -22,4 +23,3 @@ def produce(host, body):
 
 if __name__ == "__main__":
     produce("localhost", "192.168.1.44")
-
--- web/app.py
+++ web/app.py
@@ -4,40 +4,49 @@ from flask import Flask, request, render_template, redirect
 from pymongo import MongoClient
 from bson import ObjectId
 
-
 app = Flask(__name__)
 
-mongo_uri  = os.environ.get("MONGO_URI")
-db_name    = os.environ.get("DB_NAME")
+mongo_uri = os.environ.get("MONGO_URI")
+db_name = os.environ.get("DB_NAME")
 
 client = MongoClient(mongo_uri)
-mydb = client[db_name]
-mycol = mydb["routers"]
+db = client[db_name]
+routers = db["routers"]
+interface_status = db["interface_status"]
+
 
 @app.route("/", methods=["GET"])
-def main():
-    return render_template("index.html", data=mycol.find())
+def index():
+    return render_template("index.html", routers=list(routers.find()))
+
 
 @app.route("/add", methods=["POST"])
-def add_comment():
-    IP = request.form.get("IP")
-    Username = request.form.get("Username")
-    Password = request.form.get("Password")
+def add_router():
+    ip = request.form.get("ip")
+    username = request.form.get("username")
+    password = request.form.get("password")
 
-    if IP and Username and Password:
-         mycol.insert_one({"IP": IP, "Username": Username, "Password": Password})
+    if ip and username and password:
+        routers.insert_one({"ip": ip, "username": username, "password": password})
     return redirect("/")
 
-@app.route("/delete", methods=["POST"])
-def delete_comment():
-    try:
-        idx = ObjectId(request.form.get("idx"))
-        if idx:
-            mycol.delete_one({"_id" : idx})
-    except Exception:
-        pass
-    return redirect(url_for("main"))
+
+@app.route("/delete/<id>", methods=["POST"])
+def delete_router(id):
+    routers.delete_one({"_id": ObjectId(id)})
+    return redirect("/")
+
+
+@app.route("/router/<ip>", methods=["GET"])
+def router_detail(ip):
+    docs = db.interface_status.find({"router_ip": ip}).sort("timestamp", -1).limit(3)
+
+    return render_template(
+        "router_detail.html",
+        router_ip=ip,
+        interface_data=docs,
+    )
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
-    
+    app.run(debug=True, host="0.0.0.0", port=8080)
